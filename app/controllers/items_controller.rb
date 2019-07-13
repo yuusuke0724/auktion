85a5568c6e6cf7b43b033f8d6fdd93e8393f159b
Kazuma <kazuma@example.com>
2019-07-13 16:47:52 +0900

    商品登録機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,23 @@ class ItemsController < ApplicationController
   def index
     @items =Item.all 
   end
-end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+    
+  end
+end 
+
